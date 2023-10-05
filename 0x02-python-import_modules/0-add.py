b868f0bfd0d9d03e90889e9acd9fa595887f39ef
R_HYTE <polycarpwathuta39@gmail.com>
2023-10-05 18:02:06 +0300

    Fixed the pycodestyle

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -4,8 +4,8 @@ if __name__ == "__main__":
     a = 1
     b = 2
 
-    #Import the add function from add_0.py
+    # Import the add function from add_0.py
     from add_0 import add
 
-    #Call the add function and print the result using the specified format
+    # Call the add function and print the result using the specified format
     print("{} + {} = {}".format(a, b, add(a, b)))

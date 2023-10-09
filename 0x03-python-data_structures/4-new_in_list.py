242900f81e87ff3aa895abbdfbbeb07665a199a0
R_HYTE <polycarpwathuta39@gmail.com>
2023-10-09 21:13:25 +0300

    pycodestyle dictates 2 spaces are needed before beginning of in-line comment

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,7 +2,7 @@
 def new_in_list(my_list, idx, element):
     # Check if idx is negative or out of range
     if idx < 0 or idx > (len(my_list) - 1):
-        return my_list # Returns a copy of the original list
+        return my_list  # Returns a copy of the original list
 
     # Create a copy of the original list
     new_list = [i for i in my_list]

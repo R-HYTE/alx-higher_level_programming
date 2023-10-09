17ac3f31cd6d42537d9f31d03cdbc750c8a516d3
R_HYTE <polycarpwathuta39@gmail.com>
2023-10-09 20:36:32 +0300

    Introduced an edge case

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    rev_list = my_list[::-1]
+    if isinstance(my_list, list):
+        rev_list = my_list[::-1]
     for element in rev_list:
-        print("{}".format(element))
+        print("{:d}".format(element))

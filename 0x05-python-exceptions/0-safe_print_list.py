015190cfa6243b4f97b2feaa6fda5bb40310a739
R_HYTE <polycarpwathuta39@gmail.com>
2023-10-23 19:48:26 +0300

    function that prints x elements of a list
    E722 do not use bare 'except' fixed

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,7 +7,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{}".format(my_list[i]), end="")
             printed_elements += 1
-        except:
+        except IndexError:
             continue
     print()
     return printed_elements

16c78d18069c5cec633464068003632b12ed5736
R_HYTE <polycarpwathuta39@gmail.com>
2023-10-23 21:35:11 +0300

    function that raises a type exception

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+The message provided to the TypeError constructor,
+is used to provide additional information about the nature of the exception.
+"""
+
+
+def raise_exception():
+    raise TypeError("Exception raised")

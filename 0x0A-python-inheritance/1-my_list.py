ab8295166e19d77e7f67bd2db0b24a0de21eb6ee
R_HYTE <polycarpwathuta39@gmail.com>
2023-11-06 23:29:56 +0300

    method that prints the list in ascending order

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -8,6 +8,7 @@ Module that inherits from List class
 
 class MyList(list):
     """Class that inherits from list class"""
+    pass
 
     def print_sorted(self):
         """Method that prints a list in ascending order"""

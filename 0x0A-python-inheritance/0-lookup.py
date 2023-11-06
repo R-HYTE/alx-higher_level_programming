aa8d29340e28c4d227ec6140ae23ae4e210d1861
R_HYTE <polycarpwathuta39@gmail.com>
2023-11-06 20:23:21 +0300

    using the dir() to return the list of names in the namespace associated with an object

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+=====================================================================
+Module that returns a list of names in the namespace of an obj passed
+=====================================================================
+"""
+def lookup(obj):
+    """Returns a list of available attributes and methods of an object."""
+    return dir(obj)

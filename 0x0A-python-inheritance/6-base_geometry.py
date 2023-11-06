13fc1802575e50b717be77af47f88126d2d06bf3
R_HYTE <polycarpwathuta39@gmail.com>
2023-11-07 00:30:13 +0300

    Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+===============================
+Module with class BaseGeometry
+===============================
+"""
+
+
+class BaseGeometry:
+    """BaseGeometry class"""
+
+    def area(self):
+        """Calculates area"""
+
+        raise Exception("area() is not implemented")

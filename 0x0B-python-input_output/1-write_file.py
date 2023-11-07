2aa3f12f3577cfc2fa76c2e5075f5bff8ba41266
R_HYTE <polycarpwathuta39@gmail.com>
2023-11-07 16:48:35 +0300

    writes a string to a text file (UTF8) and returns the number of characters written

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,6 +5,7 @@ This module shows capability of writing to a text file
 ======================================================
 """
 
+
 def write_file(filename="", text=""):
     """ writes a string to a text file"""
 

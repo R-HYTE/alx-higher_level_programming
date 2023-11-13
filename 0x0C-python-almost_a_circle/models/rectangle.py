8d3d2ade4e3aeec75798aafdfab4145c08ee8109
R_HYTE <polycarpwathuta39@gmail.com>
2023-11-13 18:25:37 +0300

    Update to include a function that returns the area of the rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -13,42 +13,60 @@ class Rectangle(Base):
         self.__x = x
         self.__y = y
 
-        @property
-        def width(self):
-            """ Rectange's width """
-            return self.__width
+    @property
+    def width(self):
+        """ Rectange's width """
+        return self.__width
 
-        @width.setter
-        def width(self, value):
-            ''' Adjusting rectangle's width '''
-            self.__width = value
+    @width.setter
+    def width(self, value):
+        ''' Adjusting rectangle's width '''
+        self.validate_integer("width", value, False)
+        self.__width = value
 
-        @property
-        def height(self):
-            """ Rectangle's height """
-            return self.__height
+    @property
+    def height(self):
+        """ Rectangle's height """
+        return self.__height
 
-        @height.setter
-        def height(self, value):
-            ''' Adjusting rectangle's height '''
-            self.__height = value
+    @height.setter
+    def height(self, value):
+        ''' Adjusting rectangle's height '''
+        self.validate_integer("height", value, False)
+        self.__height = value
 
-        @property
-        def x(self):
-            """ Accessing the value of x """
-            return self.__x
+    @property
+    def x(self):
+        """ Accessing the value of x """
+        return self.__x
 
-        @x.setter
-        def x(self, value):
-            ''' Adjusting the value of x '''
-            self.__x = value
+    @x.setter
+    def x(self, value):
+        ''' Adjusting the value of x '''
+        self.validate_integer("x", value)
+        self.__x = value
 
-        @property
-        def y(self):
-            """ Accessing the value of y """
-            return self.__y
+    @property
+    def y(self):
+        """ Accessing the value of y """
+        return self.__y
 
-        @y.setter
-        def y(self, value):
-            ''' Adjsuting the value of y '''
-            self.__y = value
+    @y.setter
+    def y(self, value):
+        ''' Adjusting the value of y '''
+        self.validate_integer("y", value)
+        self.__y = value
+
+    def validate_integer(self, name, value, eq=True):
+        ''' Validates value of attributes '''
+        if type(value) != int:
+            raise TypeError(f"{name} must be an integer")
+        if eq and value < 0:
+            raise ValueError(f"{name} must be >= 0")
+        elif not eq and value < 0:
+            raise ValueError(f"{name} must be >= 0")
+
+    def area(self):
+        ''' Computes the area of the rectangle '''
+        return self.width * self.height
+

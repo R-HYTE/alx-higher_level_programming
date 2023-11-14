a28886fd1dc2638b088f396d02e37b2010cb6e8e
R_HYTE <polycarpwathuta39@gmail.com>
2023-11-14 13:59:26 +0300

    Added a method that returns dictionary rep of a Rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -98,3 +98,13 @@ class Rectangle(Base):
             # If **kwargs is not empty, update attributes
             for key, value in kwargs.items():
                 setattr(self, key, value)
+
+    def to_dictionary(self):
+        """ Returns the dictionary representation of a Rectangle """
+        return {
+                'x': self.x,
+                'y': self.y,
+                'id': self.id,
+                'height': self.height,
+                'width': self.width
+                }

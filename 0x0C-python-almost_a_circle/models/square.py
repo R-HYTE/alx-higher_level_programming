ea33b210b0e1f54bb8a11d87aab92fbc06d37dbc
R_HYTE <polycarpwathuta39@gmail.com>
2023-11-14 13:26:26 +0300

    class Square that inherits from Rectangle

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -53,4 +53,4 @@ class Square(Rectangle):
 
     def to_dictionary(self):
         '''Returns dictionary representation of this class.'''
-s        return {"id": self.id, "width": self.width, "height": self.height, , "x": self.x, "y": self.y}
+        return {"id": self.id, "width": self.width, "height": self.height, "x": self.x, "y": self.y}

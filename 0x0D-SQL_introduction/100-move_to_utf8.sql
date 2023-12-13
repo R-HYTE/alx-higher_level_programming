5261b433a38327c30a3cded5e86be155eaec28b1
R_HYTE <polycarpwathuta39@gmail.com>
2023-12-14 01:52:56 +0300

    syntax

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,7 +1,7 @@
 -- This script converts hbtn_0c_0 database to UTF8
 -- Convert the database to UTF8MB4
 ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
-USE hbtn_0c_0
+USE hbtn_0c_0;
 -- Convert the table to UTF8MB4
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 

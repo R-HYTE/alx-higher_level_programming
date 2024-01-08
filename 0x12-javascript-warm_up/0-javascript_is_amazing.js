db15880190699fdae242cbd5554d1ee8a7c9c15d
R_HYTE <polycarpwathuta39@gmail.com>
2024-01-08 21:46:35 +0300

    First constant, first print

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-// Declare a constant variable with a descriptive name
+// Declare a const variable with a descriptive name
 const myVar = 'JavaScript is amazing';
 
 // Print the message to the console

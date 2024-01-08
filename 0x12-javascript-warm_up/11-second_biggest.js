88f142164df42aaffa7aa068e0a4558bbc0a1b36
R_HYTE <polycarpwathuta39@gmail.com>
2024-01-09 00:07:35 +0300

    searches the second biggest integer in the list of arguments

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+// Searches the second biggest integer in the list of arguments
+if (process.argv.length <= 3) {
+  console.log(0);
+} else {
+  const list = process.argv.slice(2).map(Number);
+  const sortedList = list.sort((a, b) => b - a);
+  console.log(sortedList[1]);
+}

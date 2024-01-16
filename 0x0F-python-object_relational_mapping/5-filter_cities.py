c78d34a37c5bd9c64e2f0ab0347221fe010e475b
R_HYTE <polycarpwathuta39@gmail.com>
2024-01-17 00:18:18 +0300

    script that takes in the name of a state as an argument and lists all cities of that state, using the database hbtn_0e_4_usa

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+Script to list all cities of a specific state from the hbtn_0e_4_usa database.
+
+Usage: ./5-filter_cities.py <username> <password> <database> <state_name>
+"""
+
+import sys
+import MySQLdb
+
+
+def filter_cities_by_state():
+    """
+    Connects to a MySQL server and retrieves and displays all cities
+    of a specific state from the database (SQL injection free).
+    """
+    if len(sys.argv) != 5:
+        print("Usage: {} <username> <password> <database> <state_name>"
+              .format(sys.argv[0]))
+        sys.exit(1)
+
+    username, password, database, state_name = sys.argv[1:]
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                         passwd=password, db=database)
+    cursor = db.cursor()
+
+    query = """
+        SELECT GROUP_CONCAT(cities.name ORDER BY cities.id) AS city_list
+        FROM cities
+        JOIN states ON cities.state_id = states.id
+        WHERE states.name = %s;
+    """
+    cursor.execute(query, (state_name,))
+
+    result = cursor.fetchone()
+    city_list = result[0] if result else None
+    print(city_list)
+
+    if db:
+        db.close()
+
+
+if __name__ == "__main__":
+    filter_cities_by_state()

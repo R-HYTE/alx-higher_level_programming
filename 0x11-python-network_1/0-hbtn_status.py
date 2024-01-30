6fc3222fe80f0039bf9a9fff4c14ea614156b4b3
R_HYTE <polycarpwathuta39@gmail.com>
2024-01-30 23:23:22 +0300

    style: Maintained pycodestyle standard

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
+
 """
 This script fetches the content from a specified URL using the urllib package.
 """
 
 import urllib.request
-import logging
 
-URL = 'https://alx-intranet.hbtn.io/status'
 
 def fetch_url_content(url):
     """
@@ -19,18 +18,21 @@ def fetch_url_content(url):
         bytes: The content of the URL as bytes, or None if there was an error.
     """
     try:
-        with urllib.request.urlopen(url) as response:
+        req = urllib.request.Request(url)
+        with urllib.request.urlopen(req) as response:
             return response.read()
 
     except urllib.error.URLError as e:
-        logging.error(f"Error during request to {url}: {e}")
+        print(f"Error during request to {url}: {e}")
         return None
+
 
 def main():
     """
     Main function to execute the script.
     """
-    content = fetch_url_content(URL)
+    url = 'https://alx-intranet.hbtn.io/status'
+    content = fetch_url_content(url)
 
     if content is not None:
         print('Body response:')
@@ -38,6 +40,6 @@ def main():
         print(f"\t- content: {content}")
         print(f"\t- utf8 content: {content.decode('utf-8')}")
 
+
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.ERROR)
     main()

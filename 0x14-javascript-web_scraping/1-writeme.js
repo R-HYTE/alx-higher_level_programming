48afaa8a91991d1d3ac7585d10e0f6a76c8d2ffc
R_HYTE <polycarpwathuta39@gmail.com>
2024-02-21 23:26:08 +0300

    style: fix semistandard validation

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -5,12 +5,12 @@ const filePath = process.argv[2];
 const contentToWrite = process.argv[3];
 
 if (!filePath || !contentToWrite) {
-	console.error('Usage: node 1-writeme.js <file-path> <content-to-write>');
-	process.exit(1);
+  console.error('Usage: node 1-writeme.js <file-path> <content-to-write>');
+  process.exit(1);
 }
 
 fs.writeFile(filePath, contentToWrite, 'utf-8', (err) => {
-	if (err) {
-		console.error(err);
-	}
+  if (err) {
+    console.error(err);
+  }
 });

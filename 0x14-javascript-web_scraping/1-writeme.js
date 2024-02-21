2dd3bffbfdfcf48996036f387e137f7192031852
R_HYTE <polycarpwathuta39@gmail.com>
2024-02-21 23:39:27 +0300

    script that writes a string to a file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,11 +4,6 @@ const fs = require('fs');
 const filePath = process.argv[2];
 const contentToWrite = process.argv[3];
 
-if (!filePath || !contentToWrite) {
-  console.log('Usage: node 1-writeme.js <file-path> <content-to-write>');
-  process.exit(1);
-}
-
 fs.writeFile(filePath, contentToWrite, 'utf-8', (err) => {
   if (err) {
     console.log(err);
